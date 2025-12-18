3238a57359bf2156b443484cb43e143325325f5d
allencai90 <cyychad@gmail.com>
2025-12-18 16:08:03 +0800

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,17 +1,14 @@
+
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  build: {
-    outDir: 'dist',
-    rollupOptions: {
-      input: {
-        main: './index.html'
-      }
-    }
-  },
   server: {
     port: 3000,
+  },
+  // 关键：将环境变量注入到前端代码中
+  define: {
+    'process.env.API_KEY': JSON.stringify(process.env.API_KEY)
   }
 });
